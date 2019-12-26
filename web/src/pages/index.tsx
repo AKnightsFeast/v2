a4aa3511338ebd66c165b293dee8ee8411c7f666
RKnight <roknight@arcnight.com>
2019-12-25 23:11:48 -0500

    Added "scroll to top" for each navigation

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,14 +1,15 @@
-import * as React from "react";
+//import * as React from "react";
 //import { connect } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Switch, useLocation } from "react-router-dom";
 
 import SiteWrapper from "../components/sitewrapper";
 
 import Home from "./home";
 import Policies from "./policies";
-import Assessment from "./assessment";
 import About from "./about/cheflaura";
 import Friends from "./about/friends";
+import Assessment from "./assessment";
 import Testimonials from "./testimonials";
 
 /*
@@ -23,23 +24,47 @@ import LoggedInRoute from "../routes/LoggedInRoute";
 import LoggedOutRoute from "../routes/LoggedOutRoute";
 */
 
+function ScrollToTopOnMount() {
+  const { pathname } = useLocation();
+
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+
+  return null;
+}
+
+function RegisterFlatsomeScript() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/js/flatsome.js";
+    script.async = true;
+  
+    document.body.appendChild(script);
+  }, []);  // passing an empty array as second argument triggers the callback in useEffect only after
+           // the initial render thus replicating `componentDidMount` lifecycle behaviour
+
+  return null;
+}
+
 const Pages = () => {
   return (
-    <Switch>
-        <SiteWrapper path="/" exact={true} component={Home} />
-        <SiteWrapper path="/policies" exact={true} component={Policies} />
-        <SiteWrapper path="/about/friends" exact={true} component={Friends} />
-        <SiteWrapper path="/about/cheflaura" exact={true} component={About} />
-        <SiteWrapper path="/testimonials" exact={true} component={Testimonials} />
+    <>
+      <ScrollToTopOnMount />
+      <Switch>
+          <SiteWrapper path="/" exact={true} component={Home} />
+          <SiteWrapper path="/policies" exact={true} component={Policies} />
+          <SiteWrapper path="/about/friends" exact={true} component={Friends} />
+          <SiteWrapper path="/about/cheflaura" exact={true} component={About} />
+          <SiteWrapper path="/testimonials" exact={true} component={Testimonials} />
 
-        <Route path="/assessment" exact={true} component={Assessment} />
-        {/*
-            <LoggedOutRoute path="/" exact={true} component={Landing} />
-            <LoggedOutRoute path="/about" exact={true} component={About} />
-            <LoggedOutRoute path="/log-in" exact={true} component={LogIn} />
-            <LoggedInRoute path="/log-out" exact={true} component={LogOut} />
-        */}
-    </Switch>
+          <Route path="/assessment" exact={true} component={Assessment} />
+          {/*
+              <LoggedOutRoute path="/" exact={true} component={Landing} />
+              <LoggedOutRoute path="/about" exact={true} component={About} />
+              <LoggedOutRoute path="/log-in" exact={true} component={LogIn} />
+              <LoggedInRoute path="/log-out" exact={true} component={LogOut} />
+          */}
+      </Switch>
+    </>
   );
 };
 
