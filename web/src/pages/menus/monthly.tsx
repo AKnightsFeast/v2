9511dac17030287510bde0ec0f6455e69fcf027a
RKnight <roknight@arcnight.com>
2020-04-19 00:08:02 -0400

    Started working on new monthly menu page

--- web/src/pages/menus/monthly.tsx
+++ web/src/pages/menus/monthly.tsx
@@ -1,45 +1,49 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { YearArray, ColumnizeArray } from '../../utils';
+import { YearArray } from '../../utils';
 import { MenuMonth, ApplicationState } from '../../modules/types';
+
+import { MonthlyMenuActions } from '../../modules/enums'
 
 const Monthly: React.FC = () => {
     const menuMonths = useSelector((state: ApplicationState) => state.MonthlyMenu.MenuMonths);
     const selectedYear = useSelector((state: ApplicationState) => state.MonthlyMenu.SelectedYear);
 
-    return (
-        <div className="row row-main">
-            <div className="col large-3 medium-12">
-                <div>
-                    {/* onChange={ props.OnDateChange } className={ "browser-default" } */}
-                    <select defaultValue={ selectedYear.toString() }>
-                    {
-                        YearArray.map((year: number, index: number) => (
-                            <option key={ index } value={ year }>{ year }</option>
-                        ))
-                    }
-                    </select>
-                </div>
-                <div className="row">
-                    {
-                        ColumnizeArray<MenuMonth>(3, Array.from(menuMonths.values())).map((monthMap: Map<number, MenuMonth>, index: number) => (
-                            <div key={ index } className="large-4 medium-12">
-                            {
-                                Array.from(monthMap.values()).map((month: MenuMonth, key: number) => {
-                                    return (<div key={ key }><a className="button is-outline is-smaller" href="#">{ month.Abbr }</a></div>)
-                                })
-                            }
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
+    const dispatch = useDispatch();
+    const getMonthsForYear = useCallback(() => dispatch({ type: MonthlyMenuActions.LOAD_ACTIVE_MONTHS }), [dispatch]);
 
-            <div className="col large-9 medium-12">
-                Select a year and month to see a menu...
+    useEffect(() => {
+        dispatch({ type: MonthlyMenuActions.LOAD_MENU_DATES })
+    }, [dispatch]);
+
+    return (
+        <section className="menu-monthly">
+            <div className="content">
+                <div className="selectors">
+                    <div className="years">
+                        <select value={ `${selectedYear}` } onChange={getMonthsForYear}>
+                        {
+                            YearArray.map((year: number, index: number) => (
+                                <option key={ index } value={ year }>{ year }</option>
+                            ))
+                        }
+                        </select>
+                    </div>
+                    <div className="months">
+                        {
+                            Array.from(menuMonths.values()).map((month: MenuMonth, key: number) => (
+                                <div key={ key }><a className="button is-outline is-smaller" href="#">{ month.Abbr }</a></div>
+                            ))
+                        }
+                    </div>
+                </div>
+
+                <div className="menu">
+                    Select a year and month to see a menu...
+                </div>
             </div>
-        </div>
+        </section>
     );
 };
 
